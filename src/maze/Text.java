4bc288242947da480c473ae8ac76146f3659b19f
Muhammad Rifqi Priyo Susanto <muhammadrifqipriyosusanto@gmail.com>
2019-07-04 06:06:35 +0700

    Simpan daftar teks dalam satu kelas
    Jika dibutuhkan penyuntingan teks, lakukan pada kelas Text.

---
+++ src/maze/Text.java
@@ -0,0 +1,21 @@
+package maze;
+
+public class Text {
+	public static final String MENU = "Menu";
+	public static final String BACK = "Kembali";
+	public static final String MOVE = "Gerak";
+	public static final String ATTACKPICK = "Serang/Ambil";
+	public static final String SELECT = "Ganti Pilihan";
+	public static final String CHOOSE = "Pilih";
+	public static final String STATUS = "Status";
+	public static final String JUTSU = "Jurus";
+	public static final String OPENBAG = "Buka Tas";
+	public static final String BAG = "Tas";
+	public static final String BAGEMPTY = "Tas kosong.";
+	public static final String USEITEM = "Pakai";
+	public static final String[] ITEMS = {"Penawar", "Pil Mana"};
+	public static final String[] ITEMDESCS = {
+		"Sumber mata air yang memancarkan air suci dari dalam perut bumi. Menambah HP hero sebanyak 50% dari HP maksimum.",
+		"Gabungan rempah yang teracik secara alami yang terbuat dari temulawak, jahe, dan minyak zaitun. Menambah Mana hero sebanyak 50% dari HP maksimum."
+	};
+}
